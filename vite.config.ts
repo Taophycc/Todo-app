d82e2bccd31f0b001ad26fd9663010b4b4e7a51b
Taofeek Kassim <taophyckassim@gmail.com>
2025-09-27 15:23:21 +0100

    GitHub Pages and Vercel deployments

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Todo-app/' : '/',
+  base: process.env.VITE_DEPLOYMENT_TARGET === 'github-pages' ? '/Todo-app/' : '/',
 })
